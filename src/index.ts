c35b540ec54f00c897aca5c51f2bdd3bc8b4e1e3
Mario Beltrán Alarcón <belco90@gmail.com>
2020-02-23 20:48:27 +0100

    first approach for Mastodonte class

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
-import { loremIpsum } from 'lorem-ipsum';
+import { loremIpsum, LoremIpsum, ILoremIpsumParams } from 'lorem-ipsum';
 import MASTODONTE_WORDS from './words';
+import { IGeneratorOptions } from 'lorem-ipsum/types/src/lib/generator';
 
-export const mastodonte = (args: Object = {}): string => {
+export const mastodonte = (args: ILoremIpsumParams = {}): string => {
   return loremIpsum({
     ...args,
     words: MASTODONTE_WORDS,
   });
 };
+
+export class Mastodonte extends LoremIpsum {
+  constructor(
+    options: IGeneratorOptions = {},
+    format?: string,
+    suffix?: string
+  ) {
+    options.words = MASTODONTE_WORDS;
+    super(options, format, suffix);
+  }
+}
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,12 +1,53 @@
-import { mastodonte } from '../src/index';
+import { mastodonte, Mastodonte } from '../src/index';
 import MASTODONTE_WORDS from '../src/words';
 
 describe('mastodonte function', () => {
-  it('should generate one sentence with custom words', () => {
+  it('should generate words with default options', () => {
     const result = mastodonte();
 
     expect(result.split('. ')).toHaveLength(1);
     result.split(' ').forEach(generatedWord => {
+      const sanitizedWord = generatedWord.replace('.', '').toLowerCase();
+      expect(MASTODONTE_WORDS).toContain(sanitizedWord);
+    });
+  });
+
+  it('should generate words with custom options', () => {
+    const result = mastodonte({ count: 3 });
+
+    expect(result.split('. ')).toHaveLength(3);
+    result.split(' ').forEach(generatedWord => {
+      const sanitizedWord = generatedWord.replace('.', '').toLowerCase();
+      expect(MASTODONTE_WORDS).toContain(sanitizedWord);
+    });
+  });
+});
+
+describe('Mastodonte class', () => {
+  it('should generate words with default options', () => {
+    const mastodonteGenerator = new Mastodonte();
+
+    const result = mastodonteGenerator.generateSentences(1);
+
+    expect(result.split('. ')).toHaveLength(1);
+    result.split(' ').forEach(generatedWord => {
+      const sanitizedWord = generatedWord.replace('.', '').toLowerCase();
+      expect(MASTODONTE_WORDS).toContain(sanitizedWord);
+    });
+  });
+
+  it('should generate words with custom options', () => {
+    const mastodonteGenerator = new Mastodonte({
+      wordsPerSentence: { min: 15, max: 15 },
+    });
+
+    const result = mastodonteGenerator.generateSentences(1);
+
+    expect(result.split('. ')).toHaveLength(1);
+
+    const words = result.split(' ');
+    expect(words).toHaveLength(15);
+    words.forEach(generatedWord => {
       const sanitizedWord = generatedWord.replace('.', '').toLowerCase();
       expect(MASTODONTE_WORDS).toContain(sanitizedWord);
     });
