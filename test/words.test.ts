9ca578255be8e70f76639b8386bf29621a54eabc
Mario Beltrán Alarcón <belco90@gmail.com>
2020-02-23 20:05:25 +0100

    include more words

--- test/words.test.ts
+++ test/words.test.ts
@@ -2,8 +2,9 @@ import MASTODONTE_WORDS from '../src/words';
 import deburr from 'lodash.deburr';
 
 it('should store unique words', () => {
-  const uniqueWords = new Set(
-    MASTODONTE_WORDS.map(word => deburr(word.toLowerCase()))
+  const sanitizedWords = MASTODONTE_WORDS.map(word =>
+    deburr(word.toLowerCase())
   );
-  expect(MASTODONTE_WORDS).toHaveLength(uniqueWords.size);
+  const uniqueWords = new Set(sanitizedWords);
+  expect(sanitizedWords).toEqual(Array.from(uniqueWords));
 });
